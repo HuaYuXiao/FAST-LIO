5bbb329efc9713faffc763f5b53cfb4a670d8b95
EasonHua <1628280289@qq.com>
2024-05-03 11:42:05 +0800

    [Update] Remove `lidar_type`

--- src/preprocess.h
+++ src/preprocess.h
@@ -87,18 +87,17 @@ class Preprocess
   ~Preprocess();
 
   void process(const sensor_msgs::PointCloud2::ConstPtr &msg, PointCloudXYZI::Ptr &pcl_out);
-  void set(bool feat_en, int lid_type, double bld, int pfilt_num);
+  void set(bool feat_en, double bld, int pfilt_num);
 
   // sensor_msgs::PointCloud2::ConstPtr pointcloud;
   PointCloudXYZI pl_full, pl_corn, pl_surf;
   PointCloudXYZI pl_buff[128]; //maximum 128 line lidar
   vector<orgtype> typess[128]; //maximum 128 line lidar
   float time_unit_scale;
-  int lidar_type, point_filter_num, N_SCANS, SCAN_RATE, time_unit;
+  int point_filter_num, N_SCANS, SCAN_RATE, time_unit;
   double blind;
   bool feature_enabled, given_offset_time;
   ros::Publisher pub_full, pub_surf, pub_corn;
-    
 
   private:
   void velodyne_handler(const sensor_msgs::PointCloud2::ConstPtr &msg);
